a0c9ffed104ccb92aa970b0ecafdb6ae84cee858
ruwani <ruwani054@gmail.com>
2018-09-18 17:34:22 +0530

    chatie

---
+++ public/chat.js
@@ -0,0 +1,38 @@
+//connection
+
+var socket = io.connect('http://localhost:4000');
+
+//DOM
+
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
+
+//Emit messages
+
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message:message.value,
+        handle:handle.value
+    });
+
+});
+
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+});
+
+//listening events
+
+socket.on('chat',function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+
+});
+
+socket.on('typing',function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing...</em></p>';
+
+});
